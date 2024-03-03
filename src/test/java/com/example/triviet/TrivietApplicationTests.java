7728359cc26cafc69ab90752bf0cb99b1f15bb4c
cuongpq1000 <johnpham170800@gmail.com>
2024-03-03 17:04:42 +0700

    Initial commit

---
+++ src/test/java/com/example/triviet/TrivietApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.triviet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrivietApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
